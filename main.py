4aac1a13bbb63381c42cba8e4e51d9675f47f0b1
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-09-14 23:41:39 -0300

    Refactor main.py to add comments for clarity and remove preguntas.txt file
    - Added comments in main.py to explain the PDF to Markdown conversion and file saving process.
    - Deleted preguntas.txt as it is no longer needed.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from markitdown import MarkItDown
 
 md = MarkItDown()
-result = md.convert("ResumenRD.pdf")
+result = md.convert("ResumenRD.pdf")  # Convierte el PDF a Markdown, tenes que escribir el nombre del archivo PDF que queres convertir
 
 
-with open("ResumenRD.md", "w", encoding="utf-8") as archivo:
+with open("ResumenRD.md", "w", encoding="utf-8") as archivo: # Guarda el archivo, aqui volves a escribir el nombre del archivo que queres guardar, en este caso es ResumenRD.md
     archivo.write(result.text_content)
 
 print("¡Archivo guardado con éxito!")
